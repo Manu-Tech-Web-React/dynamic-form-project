61a56325c2c9d137287d1052615d0fc78be34bc2
Manuel Herrera <manu.herrera.mesen@gmail.com>
2022-09-30 08:46:59 -0600

    first commit, adding navbar

---
+++ src/components/NavbarMenu.js
@@ -0,0 +1,28 @@
+import { Container, Navbar, Nav } from "react-bootstrap";
+
+export default function NavbarMenu() {
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#!">
+            Dynamic <b>Form</b>
+          </Navbar.Brand>
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <Navbar.Text>Signed in as: </Navbar.Text>
+              <a
+                href="https://manuherreramesen506.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                {" "}
+                Manuel Herrera
+              </a>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
